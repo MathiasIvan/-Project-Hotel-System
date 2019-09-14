e7a9677f4df6e89f763db3919380d65cba31652c
danivillalba007 <danivillalba_007@hotmail.com>
2019-09-14 13:09:52 -0400

    Se agregó una clase Reserva y la clase Habitacion.

---
+++ Clases_Hotel/Habitacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Hotel
+{
+    public class Habitacion
+    {
+        public int ID_Habitacion { get; set; }
+        public int Numero { get; set; }
+        public int ID_EstadoHabitacion { get; set; }
+        public int ID_TipoHabitacion { get; set; }
+        public string Costo { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
---
+++ Clases_Hotel/Reserva.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Hotel
+{
+    public class Reserva
+    {
+        public int ID_Reserva { get; set; }
+        public DateTime FechaHoraEntrada { get; set; }
+        public DateTime FechaHoraSalida { get; set; }
+        public int ID_Habitacion { get; set; }
+        public int ID_Cliente { get; set; }
+        public int ID_Registrador { get; set; }
+        public string CostoTotal { get; set; }
+        public string Observacion { get; set; }
+
+    }
+}
