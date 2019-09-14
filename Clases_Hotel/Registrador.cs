5275ff0153afb94591f4e832c2c768f7b6b6584f
JuanSantander <emilianobenitez@gmail.com>
2019-09-14 12:29:29 -0400

    Se agredo  la clase Registrador.

---
+++ Clases_Hotel/Registrador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Hotel
+{
+    public class Registrador : Persona
+    {
+        public int ID_Registrador { get; set; }
+        public string Observacion { get; set; }
+    }
+}
