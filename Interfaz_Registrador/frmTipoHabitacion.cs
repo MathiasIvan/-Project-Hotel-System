101807b883f8e7c6cecbca9342e7e797fc590978
danivillalba007 <danivillalba_007@hotmail.com>
2019-10-22 15:10:21 -0300

    Se agrego formulario Tipo de Habitacion

---
+++ Interfaz_Registrador/frmTipoHabitacion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaz_Registrador
+{
+    public partial class frmTipoHabitacion : Form
+    {
+        public frmTipoHabitacion()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTipoHabitacion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
